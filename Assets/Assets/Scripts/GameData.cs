23f7bd9b254c7c0e632b4ceb1c69715d716f685b
Froman <agbuha@gmail.com>
2021-11-19 20:21:32 +1000

    1911 Level Fix
    Redo last level fix. Make scroll rect save.

---
+++ Assets/Assets/Scripts/GameData.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+[Serializable]
+public class SaveData
+{
+    [Header("Levels")]
+    public bool[] isActive;
+    public int[] highScores;
+    public int[] stars;
+
+    [Header("Player")]
+    public int playerHealth;
+    public int playerCoins;
+
+    [Header("Timers")]
+    public TimeSpan timeOld;
+    public float freeSpinTimer;
+    public bool isFreeSpinTimerActive = false;
+
+    public float adsSpinTimer;
+    public bool isAdsSpinTimerActive = false;
+
+    public float healthTimer;
+    public bool isHealthTimerActive = false;
+    public float secondsInFiveMinets = 300;
+
+    [Header("Busters")]
+
+    public int[] busterValue;
+
+    public float scrollPos;
+}
+
+public class GameData : MonoBehaviour
+{
+    public static GameData gameData;
+    public SaveData saveData;    
+
+    void Awake()
+    {
+        if (gameData == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            gameData = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        Load();
+    }
+
+    void Start()
+    {
+             
+    }
+
+    private void Update()
+    {
+        if (saveData.isHealthTimerActive)
+        {
+            if (saveData.healthTimer > 0)
+            {
+                saveData.healthTimer -= Time.deltaTime;
+            }
+            else if (saveData.healthTimer <= 0)
+            {
+                saveData.playerHealth++;
+                gameData.Save();
+                ResetHealthTimer();
+            }
+        }
+
+        if (saveData.playerHealth < 5)
+        {
+            StartPlayerHealthTimer();
+        }
+    }
+
+    public void StartPlayerHealthTimer()
+    {
+        saveData.isHealthTimerActive = true;
+    }
+
+    public void ResetHealthTimer()
+    {
+        saveData.isHealthTimerActive = false;
+        saveData.healthTimer = saveData.secondsInFiveMinets;
+    }
+
+    public void Save()
+    {
+        saveData.timeOld = DateTime.Now.TimeOfDay;
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/player.dat", FileMode.Create);
+        SaveData data = new SaveData();
+        data = saveData;
+        formatter.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/player.dat"))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/player.dat", FileMode.Open);
+            saveData = formatter.Deserialize(file) as SaveData;
+            file.Close();
+        }
+    }    
+}
---
+++ Assets/Assets/Scripts/ScrollManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollManager : MonoBehaviour
+{
+    private GameData gameData;
+    private ScrollRect scroll;
+
+    void Start()
+    {
+        scroll = FindObjectOfType<ScrollRect>();
+        gameData = FindObjectOfType<GameData>();
+
+        if (scroll != null)
+        {
+            scroll.verticalNormalizedPosition = gameData.saveData.scrollPos;
+        }
+    }    
+    
+    public void ScrollRectVectorSave()
+    {
+        gameData.saveData.scrollPos = scroll.verticalNormalizedPosition;
+    }
+}
